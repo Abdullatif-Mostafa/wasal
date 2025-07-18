14a15d5202841459661cf0750ffc24a16121950c
Tifa <abdellatyfmostafa8@gmail.com>
2025-07-18 10:45:57 +0200

    new changes 20

--- src/app/Pages/Saved/page.js
+++ src/app/Pages/Saved/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import { FaArrowRight, FaBookmark, FaRegFileImage, FaCheckCircle } from "react-icons/fa";
 
 const metedata={
